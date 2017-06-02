3f4271400182633af487b8843dd2ea5fbac9a649
swapnalipingale <swapnali.pingale@ecs.co.uk>
2017-06-02 12:19:21 +0530

    Update 99_mysql_setup.sh

--- my_init.d/99_mysql_setup.sh
+++ my_init.d/99_mysql_setup.sh
@@ -22,8 +22,8 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     /usr/bin/mysqladmin -u root password 'root'
 
     echo "*** Creating mysql user: $MYSQL_USER with pass: $MYSQL_PASS"
-    mysql -uroot -proot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';"
-    mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+    mysql -uroot -proot -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASS}';"
+    mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON jiradb TO '${MYSQL_USER}'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
     echo '*** Bootstrapping database with scripts found in /root/setup'
     if [ -d /root/setup ]; then
